82be8ec7f94aa5b1011850510451660c33435334
smntsng <144932270+smntsng@users.noreply.github.com>
2023-11-29 21:47:33 +0000

    api research and testing

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+// // youtube api - search endpoint
+
+
+
+// const url = 'https://youtube138.p.rapidapi.com/auto-complete/?q=desp&hl=en&gl=US';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '8412d21c6amsh6caf2d6964fecb0p154d87jsn1175739824ca',
+// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://youtube138.p.rapidapi.com/search/?q=despacito&hl=en&gl=US', options )
+//   .then(function (response) {
+//     return response.json();
+//   }).then(function (data) {
+//   console.log(data)
+//   });
+
+// youtube api - playlist endpoint
+
+// const url = 'https://youtube138.p.rapidapi.com/playlist/details/?id=PLcirGkCPmbmFeQ1sm4wFciF03D_EroIfr&hl=en&gl=US';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '8412d21c6amsh6caf2d6964fecb0p154d87jsn1175739824ca',
+// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://youtube138.p.rapidapi.com/playlist/details/?id=PLcirGkCPmbmFeQ1sm4wFciF03D_EroIfr&hl=en&gl=US', options )
+//   .then(function (response) {
+//     return response.json();
+//   }).then(function (data) {
+//   console.log(data)
+//   });
+
+  // youtube api - video endpoint
+
+// const url = 'https://youtube138.p.rapidapi.com/video/details/?id=kJQP7kiw5Fk&hl=en&gl=US';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '8412d21c6amsh6caf2d6964fecb0p154d87jsn1175739824ca',
+// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+// 	}
+// };
+
+// fetch(url, options )
+//   .then(function (response) {
+//     return response.json();
+//   }).then(function (data) {
+//   console.log(data)
+//   });
+
+
+// musicapi - public search endpoint
+
+// const url = 'https://musicapi13.p.rapidapi.com/public/search';
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/json',
+//     'X-RapidAPI-Key': '8412d21c6amsh6caf2d6964fecb0p154d87jsn1175739824ca',
+//     'X-RapidAPI-Host': 'musicapi13.p.rapidapi.com',
+//   },
+//   body: JSON.stringify({
+//     track: '',
+//     artist: 'ed sheeran',
+//     type: 'album',
+//     sources: ['youtube'],
+//   }),
+// };
+
+// fetch(url, options)
+//   .then(response => response.json())
+//   .then(data => {
+//     // Handle the response data
+//     console.log(data);
+//   })
+
+// radioworldapi - get genre endpoint
+
