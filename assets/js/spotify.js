c2120429ebd826163b9304fd7089185f6a3bfd35
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-30 17:25:48 +0000

    Added Spotify plans as comments

---
+++ assets/js/spotify.js
@@ -0,0 +1,11 @@
+//   ------------  Plan -------------
+
+// Get access token using client Id and secret
+
+// Then make an api call using token
+
+// Modify the api to get playlists
+
+// retrieve playlists
+
+// Use iframe provided by spotify to play those playlists
