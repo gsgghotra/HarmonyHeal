5b2693c568eefe42bfa5c3fa44e861036bf6daf3
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-30 20:16:37 +0000

    Spotify track working with static track id

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -7,7 +7,6 @@ var client_id = '';
 var client_secret = '';
 
 async function getToken() {
-
     const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         body: new URLSearchParams({
@@ -18,7 +17,6 @@ async function getToken() {
             'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
         },
     });
-
     return await response.json();
 }
 
@@ -46,9 +44,26 @@ async function getPlaylistInfo(access_token) {
     }
 
 // Call the API
-getToken().then(response => {
-    getPlaylistInfo(response.access_token).then(data => {
-        console.log(data)
+getToken()
+.then(response => {
+    getPlaylistInfo(response.access_token)
+.then(data => {
+        console.log(data);
+
+        //List of tracks
+        console.log("First track in the playlist", data.uri);
+
+        //Call the spotifyAPIReady function to play the track
+
+        //Create a div
+        let cardBox = document.createElement("div");
+        cardBox.classList.add("card","bg-dark","text-white");
+        let cardHead = document.createElement("h2");
+        cardHead.textContent = data.description
+        cardBox.appendChild(cardHead);
+
+        let playlistEl = document.getElementById("playlists");
+        playlistEl.appendChild(cardBox)
     })
 });
 
@@ -59,3 +74,19 @@ getToken().then(response => {
 // retrieve playlists
 
 // Use iframe provided by spotify to play those playlists
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    //
+};
+
+//Provide the uri from the chosen List
+
+
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        uri: "spotify:track:72vuBPMhwFNlSYpTSf6fVD"
+    };
+    const callback = (EmbedController) => {};
+    IFrameAPI.createController(element, options, callback);
+};
+
