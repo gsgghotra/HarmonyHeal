ce92abb8d19b8865de549b61b52dc52cd043dcad
John Skidmore <jo-hn@hotmail.co.uk>
2023-12-01 19:32:06 +0000

    attaching JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,43 @@
-const card1 = document.querySelector(".card1")
-const card2 = document.querySelector(".card2")
-const card3 = document.querySelector(".card3")
-const card4 = document.querySelector(".card4")
-const card5 = document.querySelector(".card5")
-const card6 = document.querySelector(".card6")
-const card7 = document.querySelector(".card7")
-const card8 = document.querySelector(".card8")
+// const card1 = document.querySelector(".card1")
+// const card2 = document.querySelector(".card2")
+// const card3 = document.querySelector(".card3")
+// const card4 = document.querySelector(".card4")
+// const card5 = document.querySelector(".card5")
+// const card6 = document.querySelector(".card6")
+// const card7 = document.querySelector(".card7")
+// const card8 = document.querySelector(".card8")
 
-card1.addEventListener('click' e => {
-    console.log(e)
-    }
-    )
-    card2.addEventListener('click' e => {
-        console.log(e)
-        }
-        )
-        card3.addEventListener('click' e => {
-            console.log(e)
-            }
-            )
-            card4.addEventListener('click' e => {
-                console.log(e)
-                }
-                )
-                card5.addEventListener('click' e => {
-                    console.log(e)
-                    }
-                    )
-                    card6.addEventListener('click' e => {
-                        console.log(e)
-                        }
-                        )
-                        card7.addEventListener('click' e => {
-                            console.log(e)
-                            }
-                            )
-                            card8.addEventListener('click' e => {
-                                console.log(e)
-                                }
-                                )
+// card1.addEventListener('click' e => {
+//     console.log(e)
+//     }
+//     )
+//     card2.addEventListener('click' e => {
+//         console.log(e)
+//         }
+//         )
+//         card3.addEventListener('click' e => {
+//             console.log(e)
+//             }
+//             )
+//             card4.addEventListener('click' e => {
+//                 console.log(e)
+//                 }
+//                 )
+//                 card5.addEventListener('click' e => {
+//                     console.log(e)
+//                     }
+//                     )
+//                     card6.addEventListener('click' e => {
+//                         console.log(e)
+//                         }
+//                         )
+//                         card7.addEventListener('click' e => {
+//                             console.log(e)
+//                             }
+//                             )
+//                             card8.addEventListener('click' e => {
+//                                 console.log(e)
+//                                 }
+//                                 )
+
+console.log('Hello')
