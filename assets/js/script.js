302bf57c06930a0c8ee58840891bc14ae9c92f66
John Skidmore <jo-hn@hotmail.co.uk>
2023-12-01 19:19:35 +0000

    Redone the event listener and classes as it didn't work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-const card1 = document.querySelector(".card1")
-const card2 = document.querySelector(".card2")
-const card3 = document.querySelector(".card3")
-const card4 = document.querySelector(".card4")
-const card5 = document.querySelector(".card5")
-const card6 = document.querySelector(".card6")
-const card7 = document.querySelector(".card7")
-const card8 = document.querySelector(".card8")
+const cardLink1 = document.querySelector(".card1")
+const cardLink2 = document.querySelector(".card2")
+const cardLink3 = document.querySelector(".card3")
+const cardLink4 = document.querySelector(".card4")
+const cardLink5 = document.querySelector(".card5")
+const cardLink6 = document.querySelector(".card6")
+const cardLink7 = document.querySelector(".card7")
+const cardLink8 = document.querySelector(".card8")
 
 
 card1.addEventListener('click' e => {
