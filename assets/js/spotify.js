6991b6ff89ce894eb01d0305e98023f65bb43894
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-03 17:53:35 +0000

    Music Playing on clicked option

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,11 +1,9 @@
-//   ------------  Plan -------------
-
 // Get access token using client Id and secret
     //https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
 
 var client_id = '';
 var client_secret = '';
-
+//Get token function - returns access_token
 async function getToken() {
     const response = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
@@ -20,16 +18,110 @@ async function getToken() {
     return await response.json();
 }
 
+// Use iframe provided by Spotify to play the track
+window.onSpotifyIframeApiReady = async (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+        // Use the provided URI or a default one
+        uri:  "spotify:track:6ygiy70ujkNOYXM1tQuMNe"  
+    };
+
+    const callback = (EmbedController) => {
+        playerControls(EmbedController)
+    };
+    await IFrameAPI.createController(element, options, callback);
+};
+
+//Manage user controls
+async function playerControls(EmbedController){
+    //Click on the play button
+    let spotifyPlay = document.querySelector('#spotifyPlay');
+    spotifyPlay.addEventListener('click', ()=>{
+        if (spotifyPlay.innerHTML === "Play") {
+            spotifyPlay.innerHTML = "Pause";
+            EmbedController.togglePlay();
+        } else {
+            spotifyPlay.innerHTML = "Play";
+            EmbedController.togglePlay();
+        }
+        
+    })
+
+    //Click on option1
+    let option1 = document.querySelector('#option1');
+
+    //Operate Serach
+    option1.addEventListener('click', async (event)=>{
+
+        // EmbedController.loadUri('spotify:track:09T2kn41rmuRgKozR3fJlH');
+        //Play after Choosing Audio source
+        // EmbedController.play();
+        let searchText;
+        console.log(event.target.dataset.search)
+        if(event.target.dataset.search){
+            searchText = event.target.dataset.search;
+            let newTrack = await fetchDataManager(searchText);
+            console.log("Lets play new Track: ", newTrack)
+            EmbedController.loadUri(newTrack);
+            EmbedController.play();
+        }
+        //fetchDataManager()
+    })
+
+    //Lets Click on Parent div of option
+    let optionSection = document.querySelector('#optionSection');
+    optionSection.addEventListener('click', event => {
+        console.log(event.target.id)
+    })
+}
+
 // Then make an api call using token
+async function fetchDataManager(searchText) {
+    if(searchText){ //If element has saerch text
+        try {
+            const response = await getToken();
+            // Run search here and return the result
+            const searchResult = await getSearchResult(response.access_token, searchText);
+            //Get Album Data 
+            const findTrackResult = await findTrack(searchResult)
+    
+            //GetTrack
+            const getTrackResult = await getAlbumInfo(findTrackResult)
+    
+            //Manage Tracks
+            const manageTrackResult = await manageTrack(getTrackResult);
+    
+            if (manageTrackResult){
+                console.log("Success");
+                return manageTrackResult;
+            }
+    
+            // Rest of the code...
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+}
 
 //This function returns track info
 async function getTrackInfo(access_token) {
-const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + access_token },
-});
+    const response = await fetch("https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT", {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+    });
 
-return await response.json();
+    return await response.json();
+}
+
+//This function searches albums
+async function getSearchResult(token, searchText) {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${searchText}&type=artist%2Calbum&limit=1`, {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + token },
+    });
+    let searchRes = response.json()
+    //console.log("Search Result: ", searchRes)
+    return searchRes
 }
 
 //This function returns Album info
@@ -42,19 +134,7 @@ async function getAlbumInfo(album) {
     return await response.json();
     }
 
-//This function searches albums
-async function getSearchResult() {
-    const token = await getToken();
-    const response = await fetch("https://api.spotify.com/v1/search?q=calmly&search?q=Sleep&type=artist%2Calbum&limit=1", {
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + token.access_token },
-    });
-    let searchRes = response.json()
-    //console.log("Search Result: ", searchRes)
-    return searchRes
-}
-
-async function setAlbumData(data){
+async function findTrack(data){
     //List of tracks
     console.log("First album from search: ", data.albums.items);
     let totalItems = data.albums.items.length
@@ -80,108 +160,6 @@ async function setAlbumData(data){
 async function manageTrack(data){
     console.log("List of Tracks: ", data.tracks.items[0].uri)
     if (data.tracks.items[0].uri){
-        return true
+        return data.tracks.items[0].uri
     }
-}
-
-async function fetchData() {
-    try {
-        const response = await getToken();
-        // Run search here and return the result
-        const searchResult = await getSearchResult(response.access_token);
-        //Get Album Data 
-        const setAlbumResult = await setAlbumData(searchResult)
-
-        //GetTrack
-        const getTrackResult = await getAlbumInfo(setAlbumResult)
-
-        //Manage Tracks
-        const manageTrackResult = await manageTrack(getTrackResult);
-
-        if (manageTrackResult){
-            console.log("Success");
-        }
-
-        // Rest of the code...
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
-}
-
-fetchData()
-
-// // Call the API
-// getToken()
-// .then(response => {
-//     //Run search here and retuen the result
-//     return getSearchResult(response.access_token);
-// })
-// .then(data => {
-//     return setAlbumData(data);
-// })
-// .then( data => {
-//     //console.log ("Lets get the album ", data)
-//     console.log(getAlbumInfo(data))
-//     return getAlbumInfo(data)
-// })
-// .then( data => {
-//     console.log("List of Tracks: ", data.tracks.items[0].uri)
-//     if(data.tracks.items[0].uri){
-//         audioPlayer("spotify:track:0vtp6L7PjGRNfbNefNWpjc")
-//     }
-    
-    
-// })
-
-// Modify the api to get playlists
-
-//https://api.spotify.com/v1/playlists/{playlist_id}
-
-// retrieve playlists
-
-
-
-
-// Use iframe provided by Spotify to play the track
-
-    window.onSpotifyIframeApiReady = async (IFrameAPI) => {
-        const element = document.getElementById('embed-iframe');
-        const options = {
-            uri:  "spotify:track:0vtp6L7PjGRNfbNefNWpjc"  // Use the provided URI or a default one
-        };
-        
-        const callback = (EmbedController) => {
-            playerControls(EmbedController)
-        };
-        await IFrameAPI.createController(element, options, callback);
-        };
-
-
-function playerControls(EmbedController){
-    //Click on the play button
-    let spotifyPlay = document.querySelector('#spotifyPlay');
-    spotifyPlay.addEventListener('click', ()=>{
-        if (spotifyPlay.innerHTML === "Play") {
-            spotifyPlay.innerHTML = "Pause";
-            EmbedController.togglePlay();
-        } else {
-            spotifyPlay.innerHTML = "Play";
-            EmbedController.togglePlay();
-        }
-        
-    })
-
-    //Click on option1
-    let option1 = document.querySelector('#option1');
-    option1.addEventListener('click', ()=>{
-        EmbedController.loadUri('spotify:track:09T2kn41rmuRgKozR3fJlH');
-        //Play after Choosing Audio source
-        EmbedController.play();
-    })
-
-    //Lets Click on Parent div of option
-    let optionSection = document.querySelector('#optionSection');
-    optionSection.addEventListener('click', event => {
-        console.log(event.target.id)
-    })
 }
