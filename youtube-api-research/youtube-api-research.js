1147a7be4f552f83bf55de3a627ec7fe7fa88924
smntsng <144932270+smntsng@users.noreply.github.com>
2023-12-04 15:48:17 +0000

    updared js to show video when button is clicked

--- youtube-api-research/youtube-api-research.js
+++ youtube-api-research/youtube-api-research.js
@@ -1,29 +1,18 @@
-// console.log("hello")
-
-// // Happy div
-// function happy(){
-//   var videoEl = document.getElementById("video");
-//   var id = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUwobzUc3z-0PrFpoRxNszXQ&key=AIzaSyA-sowA9l04T8uxLu2Shh1Wefl0ha8zego")
-//   .then(res => res.json())
-// //   .then(data => {
-//     console.log(data);
-// console.log(id)
-// }
-
 function happy(){
 var videoEl = document.getElementById("video");
-var id = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUwobzUc3z-0PrFpoRxNszXQ&key=AIzaSyA-sowA9l04T8uxLu2Shh1Wefl0ha8zego")
+fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=DgUgm2Ww1T4&key=AIzaSyA-sowA9l04T8uxLu2Shh1Wefl0ha8zego")
   .then(res => res.json())
   .then(data => {
     console.log(data);
     console.log(data.items[0].snippet.title);
     videoEl.innerText = data.items[0].snippet.title;
-    console.log(data.items[0].snippet.resourceId
-      .videoId)
-      var videoId = data.items[0].snippet.resourceId
-      .videoId
+
+      var videoId = data.items[0].id
       console.log(videoId)
-      var youtubeUrl = "https://www.youtube.com/embed/" + videoId;
+      var channelId = data.items[0].snippet.channelTitle
+
+      console.log(channelId)
+      var youtubeUrl = "https://www.youtube.com/embed/" + videoId
 
       console.log(youtubeUrl)
       var iframeElement = document.createElement('iframe');
@@ -33,10 +22,11 @@ var id = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=sni
   });
 }
 
+
 var happyButton = document.querySelector(".happy");
-var videoEL = document.getElementById("video")
+var videoEl = document.getElementById("video")
 happyButton.addEventListener('click', function(e) {
-  videoEL.innerHTML = '';
+  videoEl.innerHTML = '';
   happy();
 });
 
@@ -44,39 +34,5 @@ happyButton.addEventListener('click', function(e) {
 
 
 
-// console.log("test")
 
-
-// video IDs
-// happy id MBRCnjVecq8&ab
-// stress id BtXPQimVhg&ab
-//  anxious id 79kpoGF8KWU&ab
-// lFcSrYw-ARY&ab
-
-
-// var videoEl = document.getElementById("video");
-
-// var happyPLaylist = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=" + "PLAQ7nLSEnhWTEihjeM1I-ToPDJEKfZHZu&" + "key=AIzaSyA-sowA9l04T8uxLu2Shh1Wefl0ha8zego"
-
-// console.log(happyPLaylist)
-
-// var id = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=" + "PLAQ7nLSEnhWTEihjeM1I-ToPDJEKfZHZu&" + "key=AIzaSyA-sowA9l04T8uxLu2Shh1Wefl0ha8zego")
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data);
-//     console.log(data.items[0].snippet.title);
-//     videoEl.innerText = data.items[0].snippet.title;
-//     console.log(data.items[0].snippet.resourceId
-//       .videoId)
-//       var videoId = data.items[0].snippet.resourceId
-//       .videoId
-//       console.log(videoId)
-//       var youtubeUrl = "https://www.youtube.com/embed/" + videoId;
-
-//       console.log(youtubeUrl)
-//       var iframeElement = document.createElement('iframe');
-//       iframeElement.src = youtubeUrl;
-//       document.body.appendChild(iframeElement);
-      
-//   });
 
