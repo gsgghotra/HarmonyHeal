b28715a44b7459860e0dccef8928a595b4c23a2a
smntsng <144932270+smntsng@users.noreply.github.com>
2023-12-05 20:38:45 +0000

    updated quote api to choose search catergory based on div click

--- assets/quote-api-research/quote-api-research.js
+++ assets/quote-api-research/quote-api-research.js
@@ -1,18 +1,27 @@
-var category = 'happiness';
 var quoteEl = document.getElementById("quote")
+var h3El = document.createElement('h3');
 
 
-    fetch('https://api.api-ninjas.com/v1/quotes?category=' + category, {
-      headers: {
-          'X-Api-Key': 'ywwpzBRnPosEDtdbyOdjyQ==SbFIzsUdYwJwVLmm',
-          'Content-Type': 'application/json'
-      },
-  })
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-          console.log(data[0].quote)
-            var h3El = document.createElement('h3');
-            h3El.textContent = data[0].quote;
-            quoteEl.appendChild(h3El);
-        });
+let optionSection = document.querySelector('#optionSection');
+optionSection.addEventListener('click', async(event) => {
+  h3El.innerHTML = '';
+quoteCat = event.target.dataset.quote;
+console.log(quoteCat)
+fetch('https://api.api-ninjas.com/v1/quotes?category=' + quoteCat, {
+  headers: {
+      'X-Api-Key': 'ywwpzBRnPosEDtdbyOdjyQ==SbFIzsUdYwJwVLmm',
+      'Content-Type': 'application/json'
+  },
+})
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      console.log(data[0].quote)
+        
+        h3El.textContent = data[0].quote;
+        quoteEl.appendChild(h3El);
+
+    });
+
+})
+
