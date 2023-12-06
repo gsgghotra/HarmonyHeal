1edd8ef0f319ba59ef2f1d1b2220fc68a2252ac2
smntsng <144932270+smntsng@users.noreply.github.com>
2023-12-06 18:27:38 +0000

    updated index and indexpage2 with data-search and data-quote

---
+++ assets/quote-api-research/quote.js
@@ -0,0 +1,14 @@
+console.log("helloworld")
+
+
+
+
+
+let optionSection = document.querySelector('#optionSection');
+optionSection.addEventListener('click', async(event) => {
+
+  quoteFunc();
+});
+
+
+
